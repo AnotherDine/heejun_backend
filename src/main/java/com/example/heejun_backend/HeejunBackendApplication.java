40ae2f56a4929eb22772067cef2db01e24f978e4
J-LOOK-J <y000945@naver.com>
2024-02-13 17:43:19 +0900

    ci 확인용

--- src/main/java/com/example/heejun_backend/HeejunBackendApplication.java
+++ src/main/java/com/example/heejun_backend/HeejunBackendApplication.java
@@ -8,6 +8,7 @@ public class HeejunBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HeejunBackendApplication.class, args);
+        System.out.println("최종확인");
     }
 
 }
