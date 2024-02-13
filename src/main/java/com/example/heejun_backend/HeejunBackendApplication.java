b92ef6ff72550b4b2ce57c125a56ffd526d0a6f3
J-LOOK-J <y000945@naver.com>
2024-02-13 11:50:46 +0900

    commit

---
+++ src/main/java/com/example/heejun_backend/HeejunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.heejun_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeejunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeejunBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/heejun_backend/HeejunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heejun_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeejunBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
