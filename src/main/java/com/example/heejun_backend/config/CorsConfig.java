eced62cbf962ec70dbb8a8063381060eb9afd661
J-LOOK-J <y000945@naver.com>
2024-03-04 20:19:41 +0900

    [fix] cors 연결 (config 수정)

--- src/main/java/com/example/heejun_backend/config/CorsConfig.java
+++ src/main/java/com/example/heejun_backend/config/CorsConfig.java
@@ -2,16 +2,18 @@ package com.example.heejun_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 스프링 서버 전역적으로 CORS 설정
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 출처
+                .allowedOriginPatterns("*") // 허용할 출처
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
